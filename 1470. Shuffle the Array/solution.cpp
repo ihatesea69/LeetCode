62d3cc0fdb09faffdaefb8f35e6c38c8dd308e00
ihatesea <ihatesea69@gmail.com>
2024-06-07 13:50:30 +0700

    Bai 3

--- 1470. Shuffle the Array/solution.cpp
+++ 1470. Shuffle the Array/solution.cpp
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <string>
+
 using namespace std;
-vector<int> shuffle(vector<int>& nums, int n) {
-    vector <int> result ;
-    for(int i = 0 ; i < nums.size()/2;i++){
-        result.push_back(nums[i]);
-        result.push_back(nums[i+n]);
+
+vector<int> findWordsContaining(vector<string>& words, char x) {
+    vector<int> result = {};
+
+    for (int i = 0; i < words.size(); i++) {
+        vector<char> temp;
+        for (int k = 0; k < words[i].length(); k++) {
+            temp.push_back(words[i][k]);
+        }
+        for (int j = 0; j < words[i].length(); j++) {
+            if (temp[j] == x) {
+                result.push_back(i);
+                break;
+            }
+        }
     }
+
     return result;
-    
 }
-int main(){
-    int n = 3;
-    vector <int> nums =  {2,5,1,3,4,7};
-    vector <int> out =  shuffle(nums,n);
-    for(int i = 0 ;i < out.size();i++){
-        cout << out[i] << endl;
+
+int main() {
+    vector<string> test123 = {"abc", "bcd", "aaaa", "cbc"};
+    char n = 'z';
+    vector<int> outputz = findWordsContaining(test123, n);
+
+    for (int i = 0; i < outputz.size(); i++) {
+        cout << outputz[i] << endl;
     }
+
     return 0;
 }
